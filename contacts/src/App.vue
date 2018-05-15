a087e33aef730060f0da8660721fd01c2f5dc06d
school8478 <school8478@naver.com>
2018-05-15 18:11:02 +0900

    vuejs

--- contacts/src/App.vue
+++ contacts/src/App.vue
@@ -6,7 +6,7 @@
     </div>
     <component v-bind:is="currentView" v-bind:contact="contact"></component>
     <contactList v-bind:contactlist="contactlist"></contactList>
-    <paginate ref="pagebuttons" v-bind:page-count="totalpage" v-bind:page-range="10" v-bind:margin-pages="2" v-bind:click-handler="pageChanged" v-bind:prev-text="'<'" v-bind:next-text="'>'" v-bind:container-class="'pagination'" v-bind:page-class="'page-item'"></paginate>
+    <paginate ref="pagebuttons" v-bind:page-count="totalpage" v-bind:page-range="5" v-bind:margin-pages="2" v-bind:click-handler="pageChanged" v-bind:prev-text="'<'" v-bind:next-text="'>'" v-bind:container-class="'pagination'" v-bind:page-class="'page-item'"></paginate>
   </div>
 </template>
 
@@ -23,44 +23,125 @@ import EventBus from './eventBus.js';
 import Paginate from 'vuejs-paginate';
 
 export default {
-  name : 'app',
-  components : {
-    ContactList,
-    AddContact,
-    UpdateContact,
-    UpdatePhoto,
-    Paginate
-  },
-  data : function() {
-    return {
-      currentView : null,
-      contact : {
-        no : 0,
-        name : '',
-        tel : '',
-        address : '',
-        photo : ''
-      },
-      contactlist : {
-        pageno : 1,
-        pagesize : CONF.PAGESIZE,
-        tatalcount : 0,
-        contacts : []
-      }
+    name : 'app',
+        components : {
+        ContactList,
+        AddContact,
+        UpdateContact,
+        UpdatePhoto,
+        Paginate
+    },
+    data : function() {
+        return {
+            currentView : null,
+            contact : {
+                no : 0,
+                name : '',
+                tel : '',
+                address : '',
+                photo : ''
+            },
+            contactlist : {
+                pageno : 1,
+                pagesize : CONF.PAGESIZE,
+                tatalcount : 0,
+                contacts : []
+            }
+        }
+    },
+    mounted : function() {
+        this.fetchContacts();
+        eventBus.$on("cancel", () => {
+            this.currentView = null;
+        });
+        eventBus.$on("addSubmit", () => {
+            this.currentView = null;
+            this.addContact(contact);
+        });
+        eventBus.$on("updateSubmit", () => {
+            this.currentView = null;
+            this.updateContact(contact);
+        });
+        eventBus.$on("addContactForm", () => {
+            this.currentView = "addContact";
+        });
+        eventBus.$on("editContactForm", (no) => {
+            this.fetchContactOne(no);
+            this.currentView = "updateContact";
+        });
+        eventBus.$on("deleteContact", (no) => {
+            this.deleteContact(no);
+        });
+        eventBus.$on("editPhoto", (no) => {
+            if (typeof file !== "undefined") {
+                this.updatePhoto(no, file);
+            }
+            this.currentView = null;
+        });
+    },
+    computed : {
+        totalpage : function() {
+            return Nath.floor((this.contactlist.totalcount - 1) / this.contactlist.pagesize) + 1;
+        }
+    },
+    methods : {
+        pageChanged : function(page) {
+            this.contactlist.pageno = page;
+            this.fetchContacts();
+        },
+        fetchContacts : function() {
+            this.$axios.get(CONF.FETCH, {
+                param : {
+                    pageno : this.contactlist.pageno,
+                    pagesize:this.contactlist.pagesize
+                }
+            }).then((response) => {
+                this.contactlist = response.data;
+            }).catch((ex) => {
+                contsole.log("fetchContacts failed", ex);
+                this.contactlist.contacts =[];
+            })
+        },
+        addContact : function(contact) {
+            this.$axios.post(CONF.ADD, contact).then((response) => {
+                this.contactlist.pageno = 1;
+                this.fetchContacts();
+            }).catch((ex) => {
+                console.log("addContact failed : ", ex);
+            })
+        },
+        updateContact : function(contact) {
+            this.$axios.put(CONF.UPDATE.replace("${no}", contact.no), contact).then((response) => {
+                this.fetchContacts();
+            })
+        },
+        fetchContactOne : function(no) {
+            this.$axios.get(CONF.FETCH_ONE.replace("${no}", no)).then((response) => {
+                this.contact = response.data;
+            }).catch((ex) => {
+                console.log("fetchContactOne failed", ex);
+            })
+        },
+        deleteContact : function(no) {
+            this.$axios.delete(CONF.DELETE.replace("${no}", no)).then((response) => {
+                this.fetchContacts();
+            })
+        },
+        updatePhoto : function(no, file) {
+            var data = new FormData();
+            data.append("photo", file);
+            this.$axios.post(CONF.UPDATE_PHOTO.replace("${no}", no), data).then((response) => {
+                this.fetchContacts();
+            }).catch((ex) => {
+                console.log("updatePhoto failed", ex);
+            })
+        }
+    },
+    watch : {
+        ["contactlist.pageno"] : function() {
+            this.$refs.pagebuttons.selected = this.contactlist.pageno - 1;
+        }
     }
-  },
-  mounted : function() {
-
-  },
-  computed : {
-
-  },
-  methods : {
-
-  },
-  watch : {
-
-  }
 }
 </script>
 
