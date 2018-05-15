f793d96acd7510664533df20f05639a8873023e3
school8478 <school8478@naver.com>
2018-05-15 09:57:06 +0900

    vuejs

--- dynamic/src/components/About.vue
+++ dynamic/src/components/About.vue
@@ -2,5 +2,92 @@
     <div>
         <h1>About</h1>
         <h3>{{(new Date()).toTimeString()}}</h3>
+        <h4>조직도</h4>
+        <tree v-bind:subs="orgcharts"></tree>
     </div>
 </template>
+
+<script>
+    import Tree from "./Tree.vue";
+
+    export default {
+        name : "about",
+        components : {Tree},
+        data : function() {
+            return {
+                orgcharts : [
+                    {
+                        name : "(주) OpenSG",
+                        type : "company",
+                        subs : [
+                            {
+                                name : "SI 사업부",
+                                type : "division",
+                                subs : [
+                                    {
+                                        name : "SI 1팀",
+                                        type : "team"
+                                    },
+                                    {
+                                        name : "SI 2팀",
+                                        type : "team"
+                                    }
+                                ]
+                            },
+                            {
+                                name : "BI 사업부",
+                                type : "division",
+                                subs : [
+                                    {
+                                        name : "BI 1팀",
+                                        type : "team"
+                                    },
+                                    {
+                                        name : "BI 2팀",
+                                        type : "team"
+                                    },
+                                    {
+                                        name : "BI 3팀",
+                                        type : "team"
+                                    }
+                                ]
+                            },
+                            {
+                                name : "솔루션 사업부",
+                                type : "division",
+                                subs : [
+                                    {
+                                        name : "ESM팀",
+                                        type : "team"
+                                    },
+                                    {
+                                        name : "MTS팀",
+                                        type : "team"
+                                    },
+                                    {
+                                        name : "ASF팀",
+                                        type : "team"
+                                    }
+                                ]
+                            },
+                            {
+                                name : "총무팀",
+                                type : "division"
+                            },
+                            {
+                                name : "인사팀",
+                                type : "division"
+                            }
+                        ]
+                    }    
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+    li.company {color:#0000ff;}
+    li.division {color:#4682b4;}
+    li.team {color:tomato;}
+</style>
